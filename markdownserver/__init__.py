45a49263b0fdb18ca0e135939ae13886df70251b
Jason Dunsmore <53437+jasondunsmore@users.noreply.github.com>
2022-08-16 17:50:23 -0500

    Add command line arguments for --host, --port, and --debug.

--- markdownserver/__init__.py
+++ markdownserver/__init__.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import
+import argparse
 from bottle import route, run, static_file
 from .markdown_converter import MarkdownConverter
 from .env import root_path, ms_host, ms_port, ms_debug
 import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host')
+parser.add_argument('--port')
+parser.add_argument('--debug', action='store_true')
+args = parser.parse_args()
 
 converter = MarkdownConverter()
 
@@ -22,9 +30,9 @@ def gfmize(resource):
 
 
 def main():
-    run(host=ms_host,
-        port=ms_port,
-        debug=ms_debug,
+    run(host=args.host or ms_host,
+        port=args.port or ms_port,
+        debug=args.debug or ms_debug,
         reloader=False)
 
 
